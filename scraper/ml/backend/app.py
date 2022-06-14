4356812d12f421cf5389dd859a2f4f746a89883c
Stefan Teslic <stefant98@gmail.com>
2022-06-15 00:51:03 +0200

    Initial knn done, added feature extraction. Have to test

--- scraper/ml/backend/app.py
+++ scraper/ml/backend/app.py
@@ -2,7 +2,8 @@ import psycopg2
 from flask import Flask, Response
 from flask_cors import CORS
 import json
-
+from data_prep import knn_prep
+from models import knn
 import matplotlib.pyplot as plt
 import numpy as np
 import psycopg2
@@ -13,13 +14,17 @@ import json
 conn = psycopg2.connect("dbname=polovniautomobili user=postgres password=123")
 cur = conn.cursor()
 
-
+knn_data, knn_result = knn_prep.extract_relevant_data(conn, cur)
 
 cur.close()
 conn.close()
 
+exit()
+
 app = Flask(__name__)
 CORS(app)
+
+
 
 @app.route("/")
 def hello_world():
--- scraper/ml/backend/models/knn.py
+++ scraper/ml/backend/models/knn.py
@@ -4,16 +4,18 @@ import numpy as np
 
 
 def manhattan_distance(x, y):
-    return np.linalg.norm(x - y, ord=1, axis=1)
+    return np.linalg.norm(x - y, ord=1)
 
 
 def euclidean_distance(x, y):
-    return np.linalg.norm(x-y, ord=2, axis=1)
+    temp = np.linalg.norm(x - y, ord=2)
+    return temp
 
 
 class KNN():
-    def __init__(self, dataset: pd.DataFrame, k = None ):
+    def __init__(self, dataset: pd.DataFrame, correct_values: pd.Series, k = None ):
         self.dataset = dataset
+        self.correct_values = correct_values
         if k is None:
             self.k = math.floor(math.sqrt(dataset.shape[0]))
             if self.k % 2 == 0: # must be odd
@@ -31,20 +33,20 @@ class KNN():
             return False
         self.k = k
 
-    def calculate_distances(self, dataset, prediction_data):
+    def calculate_distances(self, prediction_data):
         raise NotImplementedError(self.__class__.__name__ + '.try_something')
 
     # TODO
     def predict(self, prediction_data):
-        distances = self.calculate_distance(self.dataset, prediction_data)
+        distances = self.calculate_distances(prediction_data)
         nearest_neighbors = distances.argsort()[: self.k]
-        nearest_neighbor_price = y[nearest_neighbors]
+        nearest_neighbor_price = self.correct_values[nearest_neighbors]
         return np.bincount(nearest_neighbor_price).argmax()
 
 class EuclideanKNN(KNN):
-    def calculate_distances(self, prediction_data):
+    def calculate_distances(self,  prediction_data):
         return euclidean_distance(self.dataset, prediction_data)
 
 class ManhattanKNN(KNN):
-    def calculate_distances(self, dataset, prediction_data):
-        return manhattan_distance(dataset, prediction_data)
+    def calculate_distances(self,  prediction_data):
+        return manhattan_distance(self.dataset, prediction_data)
